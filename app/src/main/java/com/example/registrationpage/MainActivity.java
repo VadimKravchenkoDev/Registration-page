7516da90a399c80932d158f6c9dba79f67c73947
Notebook_Vadim <kravchenko.vadim.dev@gmail.com>
2024-12-30 12:57:51 +0200

    create binding

--- app/src/main/java/com/example/registrationpage/MainActivity.java
+++ app/src/main/java/com/example/registrationpage/MainActivity.java
@@ -8,17 +8,26 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.registrationpage.databinding.ActivityMainBinding;
 
+public class MainActivity extends AppCompatActivity {
+private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(binding.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+    }
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        binding = null;
     }
 }
